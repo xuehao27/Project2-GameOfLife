ce7b262ed81de3edef678ffd3fe6aa288f868268
xuehao27 <1257491365@qq.com>
2022-05-22 14:11:30 +0800

    I decompose the code into several functions. The I write the funtion prototypes.

---
+++ game.h
@@ -0,0 +1,47 @@
+#ifndef GAME_H
+#define GAME_H
+
+// State represents the state of some step
+typedef uint8_t Cell;
+struct State{
+    Cell** cells; // cells in 2D world
+    int width;  // the width of 2D world
+    int height;  // the height of 2D world
+};
+
+/**
+* Read the intial state from the configuration
+* @param configFileName configuration filename
+* @param initialState inital state
+*/
+void readConfiguration(char* configFileName, State* initialState);
+
+/**
+* Execute one step
+* @param currentState current state
+* @param nextState next state
+*/
+void oneStep(State* currentState, State* nextState);
+
+/**
+* Check if the game should terminate
+* @param currentState current state
+* @param prevState previous state
+* @param maximalStep maximal step
+*/
+bool shouldTerminate(State* currentState, State* prevState, int maximalStep);
+
+/**
+* Display current state of the game
+* @param currentState current state
+*/
+void displayCurrentState(State* currentState);
+
+/**
+* Write the final state into the output file
+* @param outputFileName the output filename
+* @param finalState final state
+*/
+void writeState(char* outputFileName, State* finalState);
+
+#endif // GAME_H
--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 #include "game.h"
 
 int main(int argc, char* argv[]){
-    if(argc != 0){
-        printf("Usage: %s <pathToConfig> <pathToOutFile>")
+    if(argc < 3){
+        printf("Usage: %s <pathToConfig> <pathToOutFile> [maximalStep]\n", argv[0]);
         exit(1);
     }
+    int maximalStep = INT_MAX;
+    if(argc > 3){
+        maximalStep = atoi(argv[3]);
+    }
+
     return 0;
 }
